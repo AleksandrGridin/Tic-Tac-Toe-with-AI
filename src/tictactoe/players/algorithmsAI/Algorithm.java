87a9de1d6d5803cd9b7b91659a34e3e2ac18374f
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-03-11 12:37:17 +0300

    optimize algorithm easy and medium

--- src/tictactoe/players/algorithmsAI/Algorithm.java
+++ src/tictactoe/players/algorithmsAI/Algorithm.java
@@ -13,4 +13,9 @@ public abstract class Algorithm {
 
     public abstract int[] getAlgorithm(char symbol);
 
+    protected int[] getRandom() {
+        int one = (random.nextInt(3) + 1) - 1;
+        int two = (random.nextInt(3) + 1) - 1;
+        return new int[] {one, two};
+    }
 }
--- src/tictactoe/players/algorithmsAI/MediumAlgorithm.java
+++ src/tictactoe/players/algorithmsAI/MediumAlgorithm.java
@@ -29,10 +29,4 @@ public class MediumAlgorithm extends Algorithm {
         }
         return null;
     }
-
-    private int[] getRandom() {
-        int one = (random.nextInt(3) + 1) - 1;
-        int two = (random.nextInt(3) + 1) - 1;
-        return new int[] {one, two};
-    }
 }
